f5084bdb841e59e26cd51bc0b9b115e1448981b6
anyepeter <petzyrockchendi@gmail.com>
2023-03-02 20:43:02 +0100

    add methods to the comment class

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+   def update_post_counter
+      User.find(author_id).increment!(:posts_counter)
+   end
+
+   def recent_comments
+    Comment.where(posts_id: 4).order(created_at: :desc).limit(5)
+   end
 end
+
 
