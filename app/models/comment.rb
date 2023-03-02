d3357f326877dce683d54b4bcac69e4360452cf5
anyepeter <petzyrockchendi@gmail.com>
2023-03-02 22:54:16 +0100

    resolve association issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :user
+  belongs_to :post, foreign_key: 'posts_id' class_name: 'Post'
+  belongs_to :author, foreign_key: 'author_id' class_name: 'User'
 
   after_save :comment_counter
+
+private
 
   def comment_counter
     Post.find(posts_id).increment!(:CommentsCounter)
