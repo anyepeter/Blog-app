14996a472e430e7f7a23b549aa3c0a4ce8649dd1
anyepeter <petzyrockchendi@gmail.com>
2023-03-09 17:47:07 +0100

    create controller for new method in post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,11 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new 
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end 
+  end
 end
