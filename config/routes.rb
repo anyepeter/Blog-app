c8427b70bf07fe2e9901660ca5c8b6716fc7373c
anyepeter <petzyrockchendi@gmail.com>
2023-03-10 11:52:35 +0100

    create the comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do 
+      resources :comments, only: [:new, :create]
+    end 
   end
 end
