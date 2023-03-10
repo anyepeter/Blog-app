74c2605c8278b1ebe79a9523eb16521446744909
anyepeter <petzyrockchendi@gmail.com>
2023-03-10 09:22:50 +0100

    add a form successfully

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,20 @@ class PostsController < ApplicationController
       format.html { render :new, locals: { post: @post } }
     end 
   end
+
+  def create 
+    post = Post.new(params.require(:post).permit(:Title, :Text)
+    .merge(author:current_user, CommentsCounter:0, LikesCounter:0))
+    respond_to do |format|
+      format.html do
+        if post.save 
+          flash[:success] = "Post created successfully"
+          redirect_to user_posts_url
+        else 
+          flash.now[:error] = "Error: Post could not be saved"
+          render :new, locals: { post: post }
+        end
+      end
+    end
+  end
 end
