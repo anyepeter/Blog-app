48d5f5474bd683bb112969f39c1010bb61989d72
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-03-17 16:21:44 +0630

    Add the api endpoint for the post and comments

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,18 @@
+class Api::CommentsController < Api::ApplicationController
+  def index
+    @comments = Post.find(params[:post_id]).comments
+    render json: @comments
+  end
+
+  def create
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create!(params.require(:comment).permit(:text)
+     .merge(author_id: @user.id, posts_id: @post.id))
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+end
